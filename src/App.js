4024b6344cd9ac09b2cad827c568fcc266cb854b
FabienThich <fabienthich30@gmail.com>
2022-10-12 14:53:56 -0400

    Default Home Page Update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from "./components/header";
 import Home from "./components/home";
 import Internship from "./components/internship";
-import {BrowserRouter as Router, Switch, Route, Routes, Redirect} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Routes, Navigate} from "react-router-dom";
 
 function App() {
   
@@ -12,8 +12,8 @@ function App() {
       <div className="App">
           <Header />
           <Routes>
-              <Route exact path='/'/>
-              <Redirect to='/home' />
+              <Route index element={<Home/>} />
+              <Route path='home' element={<Home/>} />
               <Route path='internship' element={<Internship/>} />
           </Routes>
         </div>

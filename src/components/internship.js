36aacdbb2ab1fe0e7ad3d94d71f3f63918f82cfc
FabienThich <fabienthich30@gmail.com>
2022-10-15 13:03:12 -0400

    Fixed link errors

--- src/components/internship.js
+++ src/components/internship.js
@@ -26,7 +26,7 @@ function Internship() {
                 {/** New Section **/}
                 <img className='imgformat' src="Website pic2.png"/>
                 <h2 className='subheading'>Hosting A Website</h2>
-                <p className='paragraph'>I completed the other half of the <a href="https://www.youtube.com/watch?v=RGOj5yH7evk&ab_channel=freeCodeCamp.org"><u style={{"color" : "blue"}}>Crash Course.</u> </a>This went smoother than yesterday as through research and trying out various cloning methods, HTTPS was easier to clone with as it caused less errors compared to SSH. Once I finished the crash course. I watched a how to host my own. I was successfully able to host my own <a href="https://fabienthich.github.io"><u style={{"color" : "blue"}}>website</u></a> as seen on the left. I have made some improvements to the website and I am currently working on pushing it out and updating it.</p>
+                <p className='paragraph'>I completed the other half of the <a href="https://www.youtube.com/watch?v=RGOj5yH7evk&ab_channel=freeCodeCamp.org" target="_blank"><u style={{"color" : "blue"}}>Crash Course.</u></a> This went smoother than yesterday as through research and trying out various cloning methods, HTTPS was easier to clone with as it caused less errors compared to SSH. Once I finished the crash course. I watched a <a href="https://www.youtube.com/watch?v=QyFcl_Fba-k&ab_channel=TheNetNinja" target="_blank"><u style={{"color" : "blue"}}>YouTube video</u></a> about GitHub pages and how to host my own. I was successfully able to host my own <a href="https://fabienthich.github.io"><u style={{"color" : "blue"}}>website</u></a> as seen on the left. I have made some improvements to the website and I am currently working on pushing it out and updating it.</p>
                     <p className='date'>(September 27, 2022)</p>
                 <p />
 
@@ -38,7 +38,7 @@ function Internship() {
                 {/** New Section **/}
                 <img className='imgformat' src="ToDoPic2.png"/>
                 <h2 className='subheading'>Introduction To React</h2>
-                <p className='paragraph'> I followed along a <a href="https://www.youtube.com/watch?v=hQAHSlTtcmY&ab_channel=WebDevSimplified"> <u style={{"color" : "blue"}}>React Tutorial.</u> </a>This tutorial does not cover the complex concepts of React but does cover the basics of React. I did not get to finish the tutorial, however, I did follow alongside. The tutorial demonstrated how to create a To-Do List using React as well as using npm and npx commands. The photo on the left shows a To-Do List that displays tasks in the console that the user typed into the textbox. As well, I successfully changed my default website page from my README.md file to <a href="index.html"> <u style={{"color" : "blue"}}>Home.</u></a></p>
+                <p className='paragraph'> I followed along a <a href="https://www.youtube.com/watch?v=hQAHSlTtcmY&ab_channel=WebDevSimplified"> <u style={{"color" : "blue"}}>React Tutorial.</u> </a>This tutorial does not cover the complex concepts of React but does cover the basics of React. I did not get to finish the tutorial, however, I did follow alongside. The tutorial demonstrated how to create a To-Do List using React as well as using npm and npx commands. The photo on the left shows a To-Do List that displays tasks in the console that the user typed into the textbox. As well, I successfully changed my default website page from my README.md file to <a href="/home"> <u style={{"color" : "blue"}}>Home.</u></a></p>
                     <p className='date'>(September 28, 2022)</p>     
                 <p />
 
@@ -71,7 +71,7 @@ function Internship() {
                 <br />
 
                 {/** New Section **/}
-                <img className='imgformat' src="Shortcuts.png"/>
+                <a href='shortcuts.txt' target="_blank"><img className='imgformat' src="Shortcuts.png"/></a>
                 <h2 className='subheading'>Learning About Windows Shortcuts</h2>
                 <p className='paragraph'> I started off the day of picking it up from where I left off. I successfully converted all of my <i>Internship-Logs</i> content into React format. As well I spent some time trying to resolve issues relating to React Routers and Switches. Unfortunately, I had no success but I got a lead from my mentor. Soon after, I had a meeting with my mentor to which he taught me windows shortcuts, which are essential to becoming a great software engineer. From this, I was able to navigate around my computer more effectively and efficiently without a mouse. Click image for <a href="shortcuts.txt" target="_blank"><u style={{"color" : "blue"}}>Shortcuts Text File.</u></a></p>
                     <p className='date'>(October 3, 2022)</p>     

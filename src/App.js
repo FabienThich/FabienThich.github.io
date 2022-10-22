83678ae364db9ff37e0101c5f5437804c44705ec
FabienThich <fabienthich30@gmail.com>
2022-10-22 14:07:34 -0400

    test

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Home from "./components/Home/home";
 import Internship from "./components/Internship/internship";
 import Projects from './components/Projects/projects';
 import Contact from './components/Contact/contact';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   
   return ( 
     
       <div className="App">
-        <Router>
+        
           <Header />
           <Routes>
               <Route path='/' element={<App />} />
@@ -22,7 +22,7 @@ function App() {
               <Route path='internship' element={<Internship/>} />
               <Route path='contact' element={<Contact/>} />
           </Routes>
-        </Router>
+        
       </div>
         
     

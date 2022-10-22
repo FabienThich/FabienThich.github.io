1c71b02d967b7abf4c6e35956a5cc6db43a6ae75
FabienThich <fabienthich30@gmail.com>
2022-10-22 17:34:38 -0400

    test

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ import Home from "./components/Home/home";
 import Internship from "./components/Internship/internship";
 import Projects from './components/Projects/projects';
 import Contact from './components/Contact/contact';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   
   return ( 
     
       <div className="App">
-        <Router>
+        <BrowserRouter>
           <Header />
           <Routes>
               <Route path='/' element={<App />} />
               <Route index element={<Home/>} />
-              <Route path='/home' element={<Home/>} />
-              <Route path='/projects' element={<Projects/>} />
-              <Route path='/internship' element={<Internship/>} />
-              <Route path='/contact' element={<Contact/>} />
+              <Route path='home' element={<Home/>} />
+              <Route path='projects' element={<Projects/>} />
+              <Route path='internship' element={<Internship/>} />
+              <Route path='contact' element={<Contact/>} />
           </Routes>
-        </Router>
+        </BrowserRouter>
       </div>
       
   );

cd6e7886ea994df57a807c83c4974a6e5c4ef01b
FabienThich <fabienthich30@gmail.com>
2022-10-22 15:20:20 -0400

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from "./components/Home/home";
 import Internship from "./components/Internship/internship";
 import Projects from './components/Projects/projects';
 import Contact from './components/Contact/contact';
-import NotFound from './components/NotFound/notfound';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 
@@ -23,7 +22,6 @@ function App() {
               <Route path='/projects' element={<Projects/>} />
               <Route path='/internship' element={<Internship/>} />
               <Route path='/contact' element={<Contact/>} />
-              <Route path='*' element={<NotFound/>} />
           </Routes>
         </Router>
       </div>

978dee2c169b8bc7c9be252fe9e40e0f533a760b
FabienThich <fabienthich30@gmail.com>
2022-11-13 15:35:24 -0500

    Worked on Education section

--- src/components/Education/education.js
+++ src/components/Education/education.js
@@ -11,10 +11,19 @@ function Education() {
           <h2 className="introformateducation text-md-center">High School</h2>
         </div>
         
-        <div className="row">
+        <div className="row justify-content-center">
           <div className="col-xxl-4">
-            <p>Specialist High Skills Major</p>
             <video className='UNHackVideo' src='UNHack.mp4' autoplay="true" loop='true' />
+            <p className="UNHack">Timelapse of UNHack 2022</p>
+          </div>
+
+          <div className="col-xxl-7">
+            <div className="row">
+              <p className="SHSMTitle">Specialist High Skills Major Program</p>
+              <div className="col">
+                <p className="SHSMParagraph">Text here...</p>
+              </div>
+            </div>    
           </div>
         </div>
 

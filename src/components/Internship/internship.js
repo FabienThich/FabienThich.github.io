27f25c99aab669e4f4aa8922fe5dab472c3267e3
FabienThich <fabienthich30@gmail.com>
2023-04-08 17:04:59 -0400

    Sort Logs

--- src/components/Internship/internship.js
+++ src/components/Internship/internship.js
@@ -3,6 +3,11 @@ import "./internship.css";
 import Logs from "./logs.js";
 
 function Internship() {
+  const LogsReordered = [...Logs].sort((prev, next) => next.id - prev.id);
+  Logs.forEach((current, i) => {
+    current.id = i++;
+  });
+
   return (
     <>
       <div className="container-fluid">
@@ -11,34 +16,25 @@ function Internship() {
           <h2 className="introformatintern text-md-center">Internship Logs</h2>
         </div>
 
-        {/* <div className="row text-center">
-          <div className="col-12">
-            <p>Navigate To:</p>
-            <div className="row">
-              <a href="#sept-tag">September Logs</a>
-            </div>
-            <div className="row">
-              <a href="#oct-tag">October Logs</a>
-            </div>
-            <div className="row">
-              <a href="#nov-tag">November Logs</a>
-            </div>
-          </div>
-        </div> */}
-
         <div>
-          {Logs.map((log) => {
+          {LogsReordered.map((log) => {
             return (
-              <div className="row col justify-content-center">
-                <div className="col-xxl-3 col-xl-3 col-lg-6 col-md-6 col-sm-11 col-xm-12 gy-5">
-                  <img className="imgformat" src={log.image} />
-                </div>
-                <div className="col-xxl-8 col-xl-9 col-lg-6 col-md-6 col-sm-11 col-xm-12 gy-5">
-                  <h2 className="subheading">{log.title}</h2>
-                  <p className="paragraph">{log.desc}</p>
-                </div>
-                <div className="row">
-                  <p className="date">({log.date})</p>
+              <div key={log.id}>
+                <div className="row col justify-content-center">
+                  <div className="col-xxl-3 col-xl-3 col-lg-6 col-md-6 col-sm-11 col-xm-12 gy-5">
+                    <img
+                      className="imgformat"
+                      src={log.image}
+                      alt={`Image of ${log.image}`}
+                    />
+                  </div>
+                  <div className="col-xxl-8 col-xl-9 col-lg-6 col-md-6 col-sm-11 col-xm-12 gy-5">
+                    <h2 className="subheading">{log.title}</h2>
+                    <p className="paragraph">{log.desc}</p>
+                  </div>
+                  <div className="row">
+                    <p className="date">({log.date})</p>
+                  </div>
                 </div>
               </div>
             );
